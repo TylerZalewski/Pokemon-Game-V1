4d5e8ef4af4388eafc3ee5e43ac936b6bae34c3c
TomFitz304 <115040471+TomFitz304@users.noreply.github.com>
2023-04-10 09:05:36 -0400

    Update BasicBattle.java

--- BasicBattle.java
+++ BasicBattle.java
@@ -1,8 +1,8 @@
 public class BasicBattle
 {
     public BasicBattle(BasicPokemon[] team1, BasicPokemon[] team2){
-        BasicPokemon player1 = team1[0];
-        BasicPokemon player2 = team2[0];
+        if(team1.attack)
+            team2.health-=team1[0].getAttack
 
     }
 }
